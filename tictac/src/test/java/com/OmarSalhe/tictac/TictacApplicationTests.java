77a2b85bce620c5287a52a44eb9e4f748ca2a6dd
Omar <osalhe12@gmail.com>
2024-07-12 19:13:19 -0400

    added project structure

---
+++ tictac/src/test/java/com/OmarSalhe/tictac/TictacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OmarSalhe.tictac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
