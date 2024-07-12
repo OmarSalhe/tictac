c6c8919675914dda2cf0d23655980c84f9ac9f77
Omar <osalhe12@gmail.com>
2024-07-12 18:52:57 -0400

    added error display and move validation

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 const board = document.getElementById('board');
-const cell = Array.from(document.getElementsByClassName('cell'));
+const cells = Array.from(document.getElementsByClassName('cell'));
+const BOARD_SIZE = 9;
 const PLAYER = 'X';
+const squares = new Array(BOARD_SIZE).fill(null);
 
-let squares = new Array(BOARD_SIZE).fill(null);
-
-function makeMove(event){
-    const err = document.getElementById('err-msg');
-    const move = cell.indexOf(event.target);
-    if(isLegal(move)){
-        clearError();
-        updateBoard(move, PLAYER);
-        // call computer move
-    }
-    else{
-        showError('Square is already occupied. Please press an empty square.');
-    }
-};
-
+let errorMsg = null;
 /*
     computer move:
         calls back end and gets move
@@ -25,46 +13,64 @@ function makeMove(event){
 
 // Game Logic
 
+function makeMove(event){
+    const move = cells.indexOf(event.target);
+    if(isLegal(move)){
+        clearError();
+        updateBoard(move, PLAYER);
+        // call computer move
+    }
+    else{
+        showError('Move Error. Please press an empty square');
+    }
+};
+
 function isLegal(move){
-    return move < BOARD_SIZE &&  !squares[move];
+    return move < BOARD_SIZE && move >= 0 && !squares[move];
 };
 
 // Rendering
 
 function updateBoard(move, player){
     squares[move] = player;
-    cell[move].textContent = squares[move];
+    cells[move].textContent = player;
 };
 
 function clearBoard(){
     for(let i = 0; i < BOARD_SIZE; i++){
-        cell[i].innerHTML = '';
-        squares[i] = EMPTY;
+        cells[i].innerHTML = '';
+        squares[i] = null;
     }
 };
 
 function clearError(){
-    const error = document.getElementById('error-container');
-    while(error.firstChild){
-        error.remove(error.firstChild);
+    const errorContainer = document.getElementById('error-container');
+    while(errorMsg && errorContainer.contains(errorMsg)){
+        errorContainer.removeChild(errorMsg);
+        errorMsg = null;
     }
 };
+
 function showError(msg){
     clearError();
     const errorContainer = document.getElementById('error-container');
-    const errorMsg = document.createElement('div')
-    errorMsg.textContent(msg);
-    errorMsg.classList.add('err-msg')
-    errorContainer.appendChild(errorMsg)
 
-    // initiates animation and removes error message from DOM after 3 seconds
+    // in case a message was properly handled, update its text and return
+    if(errorMsg){
+        errorMsg.textContent = msg;
+        return;
+    }
+
+    errorMsg = document.createElement('div')
+    errorMsg.textContent = msg;
+    errorMsg.classList.add('err-msg');
+    errorContainer.appendChild(errorMsg);
+
+    // initiates animation and removes error message from DOM after 1 second(s)
     setTimeout(() => {
         errorMsg.classList.add('fade-out');
-        errorMsg.addEventListener('transitionend', () => {
-            if(errorContainer){
-                errorContainer.removeChild(errorMsg);
-            }
-        })
-
-    }, 3000);
+        errorMsg.addEventListener('transitionend', clearError);
+    }, 1000);
 };
+
+cells.forEach(cell => cell.addEventListener('click', makeMove));
