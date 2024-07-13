8da1f6e2893bbeb9146083aebb1058e0cbcbe654
Omar <osalhe12@gmail.com>
2024-07-13 06:39:06 -0400

    Set up request mapping

---
+++ tictac/src/main/java/com/OmarSalhe/tictac/GameController.java
@@ -0,0 +1,43 @@
+package com.OmarSalhe.tictac;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+public class GameController {
+    @PostMapping("/status")
+    public ResponseEntity<Integer> checkStatus(@RequestBody char[] board){
+        final int X_WIN = 0, O_WIN = 1, TIE = 2, ONGOING = 3;
+        Game curState = new Game(board);
+        Integer status = null;
+        if(curState.isWinner('X')){
+            status = X_WIN;
+        }
+        else if(curState.isWinner('O')){
+            status = O_WIN;
+        }
+        if(curState.isTie()){
+            status = TIE;
+        }
+        else{
+            status = ONGOING;
+        }
+
+        return ResponseEntity.ok(status);
+    }
+
+    
+    @PostMapping("/move")
+    public ResponseEntity<Integer> makeMove(@RequestBody char[] board) {
+        // Initialize the computer with the current game state
+        Computer computer = new Computer(new Game(board));
+        // Calculate the computer's move
+        int move = computer.computerMove();
+        // Return the move wrapped in a ResponseEntity
+        return ResponseEntity.ok(move);
+    }
+}
